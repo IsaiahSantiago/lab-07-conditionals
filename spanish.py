e3b9cb6680dcacd0e5b4851797d93cb8c068b929
IsaiahSantiago <IsaiahSantiago456@gmail.com>
2023-10-16 16:03:18 -0400

    Completed Spanish work

---
+++ spanish.py
@@ -0,0 +1,10 @@
+operation = input("Give me a word in spanish...")
+
+if (operation == "gato"):
+    answer = "cat"
+elif (operation == "perro"):
+   answer = "dog"
+elif (operation == "caballo"):
+    answer = "horse"
+
+print(answer)
