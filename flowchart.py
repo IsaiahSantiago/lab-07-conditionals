5c8e80074de861de7ecc0be957ff4cb4a95e9223
IsaiahSantiago <IsaiahSantiago456@gmail.com>
2023-10-17 21:31:31 -0400

    flowchart PY incorrect workings

---
+++ flowchart.py
@@ -0,0 +1,20 @@
+myVar = input("What is your answer? yes/no")  
+if myVar == "yes":
+    myNextVar = input("What is your answer to my 2nd question? (yes/no) ")
+{
+    {
+  if myNextVar == "yes":
+    print("working")
+  elif myNextVar == "no":
+    print("not working")
+    }
+  else:
+    print("Answer my question! You didn't type yes or no.")
+else:
+  print("Error obtained")
+
+}
+
+
+
+
